7c67f21b5e954dec3bd0f6266ec7ed4bf320a41d
Tom Bandy <hello@tombandy.com>
2022-08-08 18:24:09 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const title = 'My Hacker Stories';
+
+const today = new Date();
+const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{title}</h1>
+        <h5>Reload time: {time}</h5>
+        <Search />
+        <hr />
+        <List list={stories} />
       </header>
     </div>
   );
-}
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li key={props.item.objectID}>
+    <span><a href={props.item.url}>{props.item.title}</a></span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+const Search = () => {
+  const handleChange = (event) =>{
+    console.log(event);
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
 
 export default App;
