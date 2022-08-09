1a40cdfaa63252aa3c15fc988a56b66301efd394
Tom Bandy <hello@tombandy.com>
2022-08-09 23:49:23 +0100

    added gps test
    testing gps location tracking

--- src/App.js
+++ src/App.js
@@ -6,6 +6,57 @@ const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
 const App = () => {
+  //https://love2dev.com/blog/html-geolocation/
+  if ("geolocation" in navigator) { 
+    console.log("geolocation allowed");
+  } else { 
+    console.log("geolocation not allowed");
+  }
+  /*
+  if ( navigator.geolocation ) { 
+    navigator.geolocation.getCurrentPosition( setCurrentPosition, positionError, { 
+      enableHighAccuracy: false, 
+      timeout: 15000, 
+      maximumAge: 0 
+    } );
+  } */
+  function positionError( error ) { 
+    switch ( error.code ) { 
+      case error.PERMISSION_DENIED: 
+        console.error( "User denied the request for Geolocation." ); 
+        break; 
+      case error.POSITION_UNAVAILABLE: 
+        console.error( "Location information is unavailable." ); 
+        break; 
+      case error.TIMEOUT: 
+        console.error( "The request to get user location timed out." ); 
+        break; 
+      case error.UNKNOWN_ERROR: 
+        console.error( "An unknown error occurred." ); 
+        break; 
+    }
+  }  
+
+  function setCurrentPosition( position ) { 
+    document.querySelector( '.accuracy' ).innerHTML = position.coords.accuracy; 
+    document.querySelector( '.altitude' ).innerHTML = position.coords.altitude; 
+    document.querySelector( '.altitudeAccuracy' ).innerHTML = position.coords.altitudeAccuracy; 
+    document.querySelector( '.heading' ).innerHTML = position.coords.heading; 
+    document.querySelector( '.latitude' ).innerHTML = position.coords.latitude; 
+    document.querySelector( '.longitude' ).innerHTML = position.coords.longitude; 
+    document.querySelector( '.speed' ).innerHTML = position.coords.speed;
+  }
+  let geoWatch;
+  function startWatch() { 
+    if ( !geoWatch ) { 
+      if ( "geolocation" in navigator && "watchPosition" in navigator.geolocation ) { 
+        geoWatch = navigator.geolocation.watchPosition( setCurrentPosition, positionError, { 
+          enableHighAccuracy: false, timeout: 15000, maximumAge: 0 
+        } ); 
+      } 
+    }
+  }
+  startWatch();
   const stories = [
     {
       title: 'React',
@@ -33,6 +84,15 @@ const App = () => {
         <hr />
         <List list={stories} />
       </header>
+      <p>
+        Accuracy: <span className="accuracy"></span><br/>
+        Altitude: <span className="altitude"></span><br/>
+        Altitude Accuracy: <span className="altitudeAccuracy"></span><br/>
+        Heading: <span className="heading"></span><br/>
+        Latitude: <span className="latitude"></span><br/>
+        Longitude: <span className="longitude"></span><br/>
+        Speed: <span className="speed"></span>
+      </p>
     </div>
   );
 };
@@ -46,7 +106,7 @@ const List = (props) => (
 );
 
 const Item = (props) => (
-  <li key={props.item.objectID}>
+  <li className='item-card' key={props.item.objectID}>
     <span><a href={props.item.url}>{props.item.title}</a></span>
     <span>{props.item.author}</span>
     <span>{props.item.num_comments}</span>
